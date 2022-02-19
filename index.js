cc9a3b654cf8bcaf499c363beaedc0e3d070f113
SShak <shawnshaknitz@gmail.com>
2022-02-19 18:28:16 -0500

    auth page wip

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ mongoose.connect(
 // });
 
 app.use(express.json());
-
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+// Auth register route for security
+router.post("/register", async (req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+});
+
+// Login auth route for security
+
+//TODO Server crashes when incorect info is put in.  (can't set header after they are sent to the client)
+//not sure if this is just a postman issue.  It seems to crash due to res.status().json("");  res.status removed for now
+
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+    //  !user && res.status(401).json("Wrong credentials!");
+  
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+      );
+      const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+  
+    //  OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+  
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        {expiresIn:"3d"}
+      );
+  
+      const { password, ...others } = user._doc;
+  
+      res.status(200).json({...others, accessToken});
+    } catch (err) {
+      res.status(401).json({error: err});
+    }
+  });
+
+module.exports = router
