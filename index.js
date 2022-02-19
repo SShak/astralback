79bd1fb486a23b2789290afa4c8df0545eed0f49
SShak <shawnshaknitz@gmail.com>
2022-02-19 14:05:11 -0500

    post get working starting models

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+).then(()=>console.log("yeah,boi"))
+.catch((err)=>{
+    console.log(err);
+});
+
+// app.get("/api/test", ()=>{
+//    console.log("pass dat test, boi");
+// });
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("You all good, boi!");
+});
