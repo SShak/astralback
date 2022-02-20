b7995ebe92d63f71ee3a0078dc839be8ff5c5188
SShak <shawnshaknitz@gmail.com>
2022-02-20 10:29:31 -0500

    fixed auth login server crash by adding if return to res.status

--- routes/auth.js
+++ routes/auth.js
@@ -26,34 +26,44 @@ router.post("/register", async (req,res)=>{
 //TODO Server crashes when incorect info is put in.  (can't set header after they are sent to the client)
 //not sure if this is just a postman issue.  It seems to crash due to res.status().json("");  res.status removed for now
 
-router.post("/login", async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-    //  !user && res.status(401).json("Wrong credentials!");
-  
-      const hashedPassword = CryptoJS.AES.decrypt(
-        user.password,
-        process.env.PASS_SEC
+router.post('/login', async (req, res) => {
+  try{
+      const user = await User.findOne(
+          {
+              username: req.body.username
+          }
       );
-      const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-  
-    //  OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
-  
+         
+      if (!user) return res.status(400).json("Wrong User Name");
+
+      const hashedPassword = CryptoJS.AES.decrypt(
+          user.password,
+          process.env.PASS_SEC
+      );
+
+
+      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+      const inputPassword = req.body.password;
+      
+      if (originalPassword != inputPassword) return res.status(400).json("Wrong password");
+
       const accessToken = jwt.sign(
-        {
+      {
           id: user._id,
           isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-        {expiresIn:"3d"}
+      },
+      process.env.JWT_SEC,
+          {expiresIn:"3d"}
       );
-  
-      const { password, ...others } = user._doc;
-  
+
+      const { password, ...others } = user._doc;  
       res.status(200).json({...others, accessToken});
-    } catch (err) {
-      res.status(401).json({error: err});
-    }
-  });
+
+  }catch(err){
+      res.status(500).json(err);
+  }
+
+});
 
 module.exports = router
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,26 +1,42 @@
-const jwt = require("jsonwebtoken")
 
-const verifyToken = (req,res,next)=>{
-    const authHeader = req.headers.verifyToken
-    if(authHeader){
-        jwt.verify(token, process.env.JWT_SEC, (err,user)=>{
-            if(err) res.status(403).json("Token is very uncool");
-            req.user = user;
-            next();
-        })
-    }else{
-        return res.status(401).json("Nope");
-    }
-};
+const jwt = require("jsonwebtoken");
 
-const verifyTokenAndAuth = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).json("uncooooool");
-        }
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("laaaaaaaame");
+      req.user = user;
+      next();
     });
+  } else {
+    return res.status(401).json("uh oh");
+  }
 };
 
-module.exports = {verifyToken, verifyTokenAndAuth}
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Come on, man");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("u serious right now");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
