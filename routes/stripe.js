555de0feeb5d47a1920c6794f8c11a97bbc0f5cd
SShak <shawnshaknitz@gmail.com>
2022-03-08 19:11:11 -0500

    stripetoken now works, but is exposed

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 // const stripe = require("stripe")(process.env.STRIPE_KEY);
-const KEY = process.env.STRIPE_KEY
-const stripe = require("stripe")(KEY);
+//const KEY = process.env.STRIPE_KEY
+//const stripe = require("stripe")(KEY);
+const stripe = require("stripe")('sk_test_51KVJ0ACzqQ5Y3NqGnSw27A9g6avr1Vlym05tPajxvtBbWTPYqPYqKOqS2leN41I9fB3fTUBdpJ9ks798K3Q7K99d003iIVxyKY');
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
